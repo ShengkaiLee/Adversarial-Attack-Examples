c608ba6ddce97b31231b6839c6e0a8fac97e1f86
Shengkai Li <sl4685@columbia.edu>
2021-04-08 00:13:44 -0400

    hw3

---
+++ mnist_exp.py
@@ -0,0 +1,94 @@
+import os
+
+from absl import app, flags
+from easydict import EasyDict
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from datasets import MNISTDataset
+import matplotlib.pyplot as plt
+
+class CNN(torch.nn.Module):
+    """Basic CNN architecture."""
+
+    def __init__(self, in_channels=1):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels, 64, 8, 1
+        )  # (batch_size, 3, 28, 28) --> (batch_size, 64, 21, 21)
+        self.conv2 = nn.Conv2d(
+            64, 128, 6, 2
+        )  # (batch_size, 64, 21, 21) --> (batch_size, 128, 8, 8)
+        self.conv3 = nn.Conv2d(
+            128, 128, 5, 1
+        )  # (batch_size, 128, 8, 8) --> (batch_size, 128, 4, 4)
+        self.fc1 = nn.Linear(
+            128 * 4 * 4, 128
+        )  # (batch_size, 128, 4, 4) --> (batch_size, 2048)
+        self.fc2 = nn.Linear(128, 10)  # (batch_size, 128) --> (batch_size, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.view(-1, 128 * 4 * 4)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        return x
+
+def ld_mnist():
+    """Load training and test data."""
+    train_transforms = torchvision.transforms.Compose(
+        [torchvision.transforms.ToTensor()]
+    )
+    test_transforms = torchvision.transforms.Compose(
+        [torchvision.transforms.ToTensor()]
+    )
+
+    # Load MNIST dataset
+    train_dataset = MNISTDataset(root="/tmp/data", transform=train_transforms)
+    test_dataset = MNISTDataset(
+        root="/tmp/data", train=False, transform=test_transforms
+    )
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=128, shuffle=True, num_workers=2
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=128, shuffle=False, num_workers=2
+    )
+    return EasyDict(train=train_loader, test=test_loader)
+
+
+def main():
+    # Load training and test data
+    data = ld_mnist()
+
+    net = CNN(in_channels=1)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    if device == "cuda":
+        net = net.cuda()
+    loss_fn = torch.nn.CrossEntropyLoss(reduction="mean")
+    optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
+
+    net.load_state_dict(torch.load("mnist.pt", map_location=device))
+    # Evaluate on clean and adversarial data
+    net.eval()
+    report = EasyDict(nb_test=0, correct=0, correct_fgm=0, correct_pgd=0)
+    for x, y in data.test:
+        x, y = x.to(device), y.to(device)
+        _, y_pred = net(x).max(1)  # model prediction on clean examples
+        report.nb_test += y.size(0)
+        report.correct += y_pred.eq(y).sum().item()
+    print(
+        "test acc on clean examples (%): {:.3f}".format(
+            report.correct / report.nb_test * 100.0
+        )
+    )
+
+if __name__ == "__main__":
+
+    main()
